d3819ee68498ea2c082f8bb396f1285fc11227d3
Noyan Çepikkurt <noyan.cepikkurt@fmsstech.com>
2023-04-11 11:24:35 +0300

    I added a feature to start the application directly in the logged-in state if no logout is performed, so that users don't have to constantly log in to the application.

--- BusBus/Core/SceneDelegate.swift
+++ BusBus/Core/SceneDelegate.swift
@@ -6,16 +6,23 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        let currentUser = Auth.auth().currentUser
+        if currentUser != nil {
+            let board = UIStoryboard(name: "Main", bundle: nil)
+            let tabBar = board.instantiateViewController(withIdentifier: "HomeTC") as! UITabBarController
+            window?.rootViewController = tabBar
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
